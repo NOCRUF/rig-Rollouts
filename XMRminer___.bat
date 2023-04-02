63cf01b374fff9371b498ae4f0be253be1118c3f
NOCRUF <118448383+NOCRUF@users.noreply.github.com>
2023-04-02 00:52:24 -0400

    Rename XMRminer___.bat to XMRminer.bat

--- XMRminer___.bat
+++
@@ -1,6 +0,0 @@
-ÿþ&cls
-@echo off
-set /p name="enter name:"
-set /p threads="enter number of thread used to mine:"
-cd Desktop\xmrig-6.19.1
-xmrig.exe -o gulf.moneroocean.stream:10128 -u 485UXYkesBkgMLE6y9WMmghZh4fC5T8VqdXSkXcf7dGK3vwtMrhbnRsi9UWj8d81yz7WmoLzbrZazTiSBYwgHhdtUWjEJWr -p %name% -t%threads% --cpu-no-yield
